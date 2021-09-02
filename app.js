0298520a8c13586f12a43964f501b284c06835d6
mhselim103 <mhselin103@gmail.com>
2021-09-02 12:28:14 +0600

    second commit

--- app.js
+++ app.js
@@ -13,24 +13,20 @@ const title = bookTitle => {
     const docs = bookTitle.docs;
     console.log(docs);
     docs.forEach(element => {
+        // console.log(element.cover_i);
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
          <div class="card">
-                        <img id="cover-image" src="searchImage(${element.cover_i})" class="card-img-top" alt="...">
+                        <img id="cover-image" src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="NO Image">
                         <div class="card-body">
-                            <h6 class="card-title"></h6>
-                            <p id="author">Author</p>
-                            <small>first published:1996</small>
+                            <h6 class="card-title">${element.title}</h6>
+                            <p id="author">${element.author_name?.[0]}</p>
+                            <small>${element.first_publish_year}</small>
                         </div>
                     </div>
         `;
         booklist.appendChild(div);
     });
 }
-const searchImage = imageId => {
-    const url = `https://covers.openlibrary.org/b/id/${imageId}-M.jpg`
-    fetch(url)
-    .then(res=> res.json())
-    .then(data =>console.log(data))
-}
+
